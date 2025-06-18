724973c030d30df6757e5aa93e4606d5611044e9
Ruben Hedstrom/ルーベン・ヘドストロム <ruben.hedstrom@gadelius.com>
2025-06-18 13:21:04 +0900

    1st init

---
+++ src/PhaserGame.tsx
@@ -0,0 +1,76 @@
+import { useEffect, useLayoutEffect, useRef } from 'react';
+import StartGame from './game/main';
+import { EventBus } from './game/EventBus';
+
+export interface IRefPhaserGame
+{
+    game: Phaser.Game | null;
+    scene: Phaser.Scene | null;
+}
+
+interface IProps
+{
+    currentActiveScene?: (scene_instance: Phaser.Scene) => void
+    ref?: React.Ref<IRefPhaserGame>;
+}
+
+export const PhaserGame = ({ currentActiveScene, ref }: IProps) =>
+{
+    const game = useRef<Phaser.Game | null>(null);
+
+    useLayoutEffect(() =>
+    {
+        if (game.current === null)
+        {
+
+            game.current = StartGame("game-container");
+
+            if (typeof ref === 'function')
+            {
+                ref({ game: game.current, scene: null });
+            } else if (ref)
+            {
+                ref.current = { game: game.current, scene: null };
+            }
+
+        }
+
+        return () =>
+        {
+            if (game.current)
+            {
+                game.current.destroy(true);
+                if (game.current !== null)
+                {
+                    game.current = null;
+                }
+            }
+        }
+    }, []);
+
+    useEffect(() =>
+    {
+        EventBus.addListener('current-scene-ready', (scene_instance: Phaser.Scene) =>
+        {
+            if (currentActiveScene && typeof currentActiveScene === 'function')
+            {
+                currentActiveScene(scene_instance);
+            }
+
+            if (ref && typeof ref !== 'function')
+            {
+                ref.current = { game: game.current, scene: scene_instance };
+            }
+            
+        });
+        return () =>
+        {
+            EventBus.removeListener('current-scene-ready');
+        }
+    }, [currentActiveScene]);
+
+    return (
+        <div id="game-container"></div>
+    );
+
+};
